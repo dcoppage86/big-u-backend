0db37faa9a446376372b58b0b914fc1d2b620b1e
Dakota <dakotakcoppage@me.com>
2021-07-15 10:24:03 -0600

    created users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
   resources :categories
   resources :libraries
   resources :daily_entries
+  resources :users, only: [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
