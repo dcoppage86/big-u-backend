16a79c5a3b2743840e29f6ea6332d36c86f24455
Dakota <dakotakcoppage@me.com>
2021-07-16 07:36:46 -0600

    added attributes and associations to comments serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :user_id, :user
+
+  belongs_to :user
+  has_many :daily_entries, through: :user
 end
