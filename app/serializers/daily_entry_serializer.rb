15f82e5e38835373238079c5697033e80f9b472a
Dakota <dakotakcoppage@me.com>
2021-07-16 07:41:25 -0600

    finished serializers

--- app/serializers/daily_entry_serializer.rb
+++ app/serializers/daily_entry_serializer.rb
@@ -2,4 +2,5 @@ class DailyEntrySerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :completed, :user_id, :user
 
   belongs_to :user
+  has_many :comments, through: :users
 end
