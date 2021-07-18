cef342df30e2eb07b4fc2c167ce7ba1f58b33dc5
Dakota <dakotakcoppage@me.com>
2021-07-18 15:35:03 -0600

    created index action for daily entry and updated seed file

--- app/controllers/api/v1/daily_entries_controller.rb
+++ app/controllers/api/v1/daily_entries_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::DailyEntriesController < ApplicationController
+    
+    def index
+        @daily_entries = DailyEntry.all
+        render json: @daily_entries
+    end
+
+    def create
+        @daily_entry = DailyEntry.new(params[:daily_entry])
+        if @daily_entry.save
+          render json: @daily_entry
+        else
+          render json: @daily_entry.errors, status: :unprocessable_entity
+        end
+    end
+    
+    
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dakota = User.create(first_name: "Dakota", last_name: "Coppage", childs_name: "Fynleigh", username: "dcoppage86", email: "dakotakcoppage@me.com", password: "password")
+
+dakota.daily_entries = DailyEntry.create([{ title: "Today was a good day...", content: "Today we did stuff that didnt drive me completely insane", user_id: 1 }])
