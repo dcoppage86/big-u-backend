4d764e5b6fff44a080dc6021180d898c6859b117
Dakota <dakotakcoppage@me.com>
2021-07-18 15:39:36 -0600

    created index and create for library controller

--- app/controllers/api/v1/libraries_controller.rb
+++ app/controllers/api/v1/libraries_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::LibrariesController < ApplicationController
+
+    def def index
+        @libraries = Library.all
+        render json: @libraries
+    end
+
+    def create
+        @library = Library.new(params[:library])
+        if @library.save
+          render json: @library, status: :accepted 
+        else
+            render json: @library.errors, status: :unprocessable_entity
+        end
+    end
+    
+    
 end
