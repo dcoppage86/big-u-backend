1b32419a5cb077692e5d511ada479fa5c0074179
Dakota <dakotakcoppage@me.com>
2021-07-19 23:32:22 -0600

    adding controller actions to daily entry

--- app/controllers/api/v1/daily_entries_controller.rb
+++ app/controllers/api/v1/daily_entries_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::DailyEntriesController < ApplicationController
+    before_action :get_daily_entry, only: [:update, :delete]
     
     def index
         @daily_entries = DailyEntry.all
@@ -10,10 +11,30 @@ class Api::V1::DailyEntriesController < ApplicationController
         if @daily_entry.save
           render json: @daily_entry
         else
-          render json: @daily_entry.errors, status: :unprocessable_entity
+          render json:{ @daily_entry.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @daily_entry = DailyEntry.find(params[:id])
+        if @daily_entry.update_attributes(daily_entry_params)
+          flash[:success] = "DailyEntry was successfully updated"
+          redirect_to @daily_entry
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
         end
     end
     
+    private
+
+    def daily_entry_params
+        params.require(:daily_entry).permit(:title, :content, :user_id)
+    end
+
+    def get_daily_entry
+        @daily_entry = DailyEntry.find(params[:id])
+    end
     
     
 end
