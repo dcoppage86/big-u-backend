107f0f9a6d492672004e22b5794828ca51692413
Dakota <dakotakcoppage@me.com>
2021-07-20 00:08:38 -0600

    moving on to react for now

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :childs_name, :username, :email, :collection, :books, :daily_entries, :comments
+  attributes :first_name, :last_name, :childs_name, :username, :email
+  belongs_to :collection
+  has_many :comments
+  has_many :daily_entries
+  has_many :books
 end
