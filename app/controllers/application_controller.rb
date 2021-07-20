1cd88411c9d8b4d4a96d1dccdf39c1343283a417
Dakota <dakotakcoppage@me.com>
2021-07-20 00:13:17 -0600

    added helper methods in application controller to mock user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
+    helpers do
+        def current_user
+            # mock user login
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 
 end
