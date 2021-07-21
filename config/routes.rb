78372af39d322ae40f833ce61ec9f998390f032f
Dakota <dakotakcoppage@me.com>
2021-07-20 23:36:13 -0600

    finished autorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/api/v1/login', to: "api/v1/auth#create"
+  get '/api/v1/get_current_user', to: "api/v1/auth#get_current_user"
   namespace :api do
     namespace :v1 do
       resources :collections, only: [:index]
