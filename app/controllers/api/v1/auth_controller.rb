6c45bfb51c21e0ce82a8260212eea1f0fbb33c10
Dakota <dakotakcoppage@me.com>
2021-07-20 22:54:15 -0600

    adding get_current_user to auth to establish that the current user is logged in

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,10 +2,21 @@ class Api::V1::AuthController < ApplicationController
     def create
         @user = User.find_by(username: params[:auth][:username])
 
-        if @user && @user.authenticate(params[:auth][:username])
+        if @user && @user.authenticate(params[:auth][:password])
+            session[:user_id] = @user.id
             render json: @user
         else
             render json: {error: "Invalid Credentials"}
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                error: "Not logged in"
+            }
         end
     end
 end
