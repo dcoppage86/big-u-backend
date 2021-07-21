1af7270d918ff401c71063c5747ce202ebe5f03f
Dakota <dakotakcoppage@me.com>
2021-07-20 22:29:43 -0600

    creating auth controller actions

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::AuthController < ApplicationController
     def create
+        @user = User.find_by(username: params[:auth][:username])
+
+        if @user && @user.authenticate(params[:auth][:username])
+            render json: @user
+        else
+            render json: {error: "Invalid Credentials"}
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/api/v1/login', to: "auth#create"
+  post '/api/v1/login', to: "api/v1/auth#create"
   namespace :api do
     namespace :v1 do
       resources :collections, only: [:index]
