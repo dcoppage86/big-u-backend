657329b5bb48015eaf99c01280f2f77f1e892c46
Dakota <dakotakcoppage@me.com>
2021-07-22 08:08:44 -0600

    changed controller actions to take in serialized data

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < ApplicationController
 
         if @user && @user.authenticate(params[:auth][:password])
             session[:user_id] = @user.id
-            render json: @user
+            render json: UserSerializer.new(@user)
         else
             render json: {error: "Invalid Credentials"}
         end
@@ -12,7 +12,7 @@ class Api::V1::AuthController < ApplicationController
 
     def get_current_user
         if logged_in?
-            render json: current_user
+            render json: UserSerializer.new(current_user)
         else
             render json: {
                 error: "Not logged in"
