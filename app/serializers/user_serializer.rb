8e1d1bc2b5a94943622ab41b4277dad85bfd0875
Dakota <dakotakcoppage@me.com>
2021-07-22 09:11:46 -0600

    added daily_entry serialized data to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,16 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :first_name, :last_name, :childs_name, :username, :email
-  belongs_to :collection
-  has_many :comments
-  has_many :daily_entries
-  has_many :books
+  attribute :daily_entries do |entry|
+    entry.daily_entries.map do |ent|
+      {
+        title: ent.title,
+        content: ent.content
+      }
+    end
+  end
+  # belongs_to :collection
+  # has_many :comments
+  # has_many :daily_entries, serializer: DailyEntrySerializer
+  # has_many :books
 end
