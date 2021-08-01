998879fc84b79a59d568d584b9aed512f7ebe41a
Dakota <dakotakcoppage@me.com>
2021-08-01 08:50:42 -0600

    created book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,12 +1,6 @@
 class BookSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :author, :book_url, :image_url, :user_id, :category_id, :collection_id
-  attribute :comment do |comment|
-    comment.comments.map do |com|
-      {
-        title: com.title,
-        content: com.content
-      }
-    end
-  end
+  belongs_to :category
+  belongs_to :user
 end
